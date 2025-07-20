8fe010d0016364d2b20a03467e29b95e7bd31d98
ramanji4 <ramanji181818@gmail.com>
2025-07-20 11:20:46 +0530

    jenkins-infra-dev

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -30,6 +30,9 @@ else
     echo "You are super user"
 fi
 
+sudo dnf install mysql -y
+VALIDATE $? "installing MySQL"
+
 #docker
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
